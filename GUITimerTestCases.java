bacf38965b9cc941df419b6634382130011b5052
kerwin1100gp <kerwinyadao@yahoo.com>
2016-11-10 12:21:12 -1000

    Add files via upload
    These files were created by kevin

---
+++ GUITimerTestCases.java
@@ -0,0 +1,28 @@
+/******************************
+ * TEAM ORANGE ICS414 GUI TIMER
+ * @BRYSON YOGI
+ * @KEVIN BEYDLER
+ * @KERWIN YADAO
+ * VERSION1.0
+ *****************************/
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GUITimerTestCases {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public final void updateDisplay() 
+	{
+	
+		
+		
+	}
+
+}
---
+++ GUITimerTestMainTestCases.java
@@ -0,0 +1,26 @@
+/******************************
+ * TEAM ORANGE ICS414 GUI TIMER
+ * @BRYSON YOGI
+ * @KEVIN BEYDLER
+ * @KERWIN YADAO
+ * VERSION1.0
+ *****************************/
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GUITimerTestMainTestCases {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public final void testMain() 
+	{
+	GUITimerMain.main(null);//see if the main() works
+	}
+
+}
